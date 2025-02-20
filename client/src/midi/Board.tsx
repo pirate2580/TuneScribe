3f0024490157170d82c5b5c8ac1b33183f11669b
pirate2580 <90294883+pirate2580@users.noreply.github.com>
2025-02-20 02:18:25 -0500

    iknow it works

--- client/src/midi/Board.tsx
+++ client/src/midi/Board.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect} from "react";
 import { useMidi } from "./MidiContext";
 import Note from './Note'
 import SeekBar from "./SeekBar";
